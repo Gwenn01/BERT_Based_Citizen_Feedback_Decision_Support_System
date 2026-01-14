cc2fa045ab40f32b2df55f1035101246c47fe809
Arnel Gwen Nuqui <arnelgwen@gmail.com>
2026-01-14 23:16:54 +0800

    fix

--- server/app.py
+++ server/app.py
@@ -12,9 +12,19 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 # Register Blueprints
 # -------------------------------------------------
 from routes.health_routes import health_bp
+# TEST CONNECTION
+from routes.test_db_routes import test_db_bp
+
+# ARTIFICIAL INTELLIGENCE
+from routes.ai.ai_routes import ai_bp
+# SURVEY
+from routes.survey.survey_routes import survey_bp
+
 
 app.register_blueprint(health_bp,  url_prefix="/api")
-
+app.register_blueprint(test_db_bp, url_prefix="/api")
+app.register_blueprint(ai_bp, url_prefix="/api")
+app.register_blueprint(survey_bp, url_prefix="/api")
 # -------------------------------------------------
 # Entry Point
 # -------------------------------------------------
