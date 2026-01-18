0542ce6147f0c622701c5f1df655e4f6b109b85d
Arnel Gwen Nuqui <arnelgwen@gmail.com>
2026-01-18 12:32:08 +0800

    done testing cc

--- server/services/run_tests.py
+++ server/services/run_tests.py
@@ -1,13 +1,24 @@
 from services.agent.survey_agent import analyze_survey
 from services.agent.sentiment_agent import analyze_sentiment
+from services.agent.citizen_charter_awareness import analyze_citizens_charter
 from model.get_feedback import get_weekly_feedback
 
 def run_services_tests():
     feedback = get_weekly_feedback()
     comments = []
+    cc_data = []
+    
     #print("Fetched Feedback:", feedback)
     #for f in feedback:
     #    comments.append(f["comment"])
     #result = analyze_sentiment(comments)
-    #print(result)
+    
+    # for f in feedback:
+    #     cc_data.append({
+    #         "cc1": f["cc1"],
+    #         "cc2": f["cc2"],
+    #         "cc3": f["cc3"]
+    #     })
+    # result = analyze_citizens_charter(cc_data)
+    #print(result)  
     print("Running services tests...")
