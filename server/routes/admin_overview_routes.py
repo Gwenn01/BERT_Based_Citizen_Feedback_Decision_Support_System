6e7fdff4395db751e41a9de97e8e2869321402ac
Gwenn01 <arnelgwenn@gmail.com>
2026-01-19 12:05:21 +0800

    feedback api

--- server/routes/admin_overview_routes.py
+++ server/routes/admin_overview_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify
-from controllers.admin_overview_mapper import format_dashboard_response
+from controllers.admin_overview_mapper import format_dashboard_response, format_recent_feedback
 from model.get_summary import get_latest_summary
+from model.get_feedback import get_recent_feedback
 
 admin_overview_bp = Blueprint("admin_overview", __name__)
 
@@ -19,3 +20,12 @@ def get_dashboard():
     dashboard_database["30 Monthly"] = format_dashboard_response(monthly_row)
 
     return jsonify(dashboard_database), 200
+
+
+@admin_overview_bp.route("/recent-feedback", methods=["GET"])
+def get_recent_feedback_API():
+    
+    recent_feedback = get_recent_feedback()
+    feedback = format_recent_feedback(recent_feedback)
+    
+    return jsonify(feedback), 200
